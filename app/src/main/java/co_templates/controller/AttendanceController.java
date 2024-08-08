c6df692cb580f434d0561963b98f76ba4d07f721
typhoon0678 <typhoon0678@gmail.com>
2024-08-08 16:47:30 +0900

    Repository, Controller 작성

--- app/src/main/java/co_templates/controller/AttendanceController.java
+++ app/src/main/java/co_templates/controller/AttendanceController.java
@@ -1,5 +1,6 @@
 package co_templates.controller;
 
+import co_templates.entity.Attendance;
 import co_templates.repository.AttendanceRepository;
 import co_templates.view.HRView;
 
@@ -14,11 +15,45 @@ public class AttendanceController {
         this.hrView = hrView;
     }
 
-    // 프로그램 실행
+    // 프로그램 실행 (인적 자원 관리 시스템 Controller)
     public void run() {
 
         // while 문으로 프로그램 실행 로직 작성
+
+        // hrView.selectSystemOption()
+        // switch(menu) {
+        // case("3"):
+        // handleAttendance();
+        // break; ... }
     }
 
+    // 근태 관리 Controller
+    public void handleAttendance() {
 
+        // while 문으로 근태 관리 입력 관리
+
+        // hrView.selectAttendanceOption();
+    }
+
+    // 근태 입력 Controller
+    public void insertAttendance() {
+
+        // attendanceRepository.addAttendance();
+    }
+
+    // 직원별 월별 근태 현황 Controller (선택 사항)
+    public void attendanceByEmployeeAndMonth() {
+
+
+    }
+
+    // 부서별 월별 근태 현황 Controller
+    public void attendanceByDepartmentAndMonth() {
+
+        // 부서 검색
+
+        // 부서 ID로 근태 LIST 검색
+
+        // hrView로 출력
+    }
 }
--- app/src/main/java/co_templates/view/HRView.java
+++ app/src/main/java/co_templates/view/HRView.java
@@ -1,10 +1,43 @@
 package co_templates.view;
 
+import co_templates.entity.Attendance;
+import co_templates.entity.Employee;
+
+import java.util.List;
 import java.util.Scanner;
 
 // View 와 input 을 관리하는 클래스
 public class HRView {
 
-    Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
 
+    // ==== 인적 자원 관리 시스템 ====  => 선택 번호 return
+    public String selectSystemOption() {
+
+        return null;
+    }
+
+    // ==== 근태 관리 ====  => 선택 번호 return
+    public String selectAttendanceOption() {
+
+        return null;
+    }
+
+    // ==== 근태 입력 ====  => 선택 사항
+    public Employee inputEmployee() {
+
+        return null;
+    }
+
+    // ==== 직원별 월별 근태 현황 ==== (선택사항)
+    public void attendanceByEmployeeAndMonth(Employee employee, List<Attendance> attendanceList) {
+
+
+    }
+
+    // ==== 부서별 월별 근태 현황 ====
+    public void attendanceByDepartmentAndMonth() {
+
+
+    }
 }
